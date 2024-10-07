814c71d1b0307c85a2d11fc27ddbb6d7bd1af7a9
Maija Lintunokka <maija.lintunokka@nosto.com>
2024-10-07 13:18:00 +0300

    fix: remove redundant test

--- test/init.spec.ts
+++ test/init.spec.ts
@@ -29,23 +29,5 @@ describe("main", () => {
       const expectedUrl = "https://connect.nosto.com/include/shopify-123"
       expect(document.querySelector(`script[src='${expectedUrl}']`)).not.toBeNull()
     })
-    it("should add page type element to the page if value is provided", async () => {
-      init({
-        merchantId: "shopify-123",
-        pageType: "checkout"
-      })
-
-      const el = document.querySelector(".nosto_page_type")
-      expect(el).not.toBeNull()
-      expect(el?.textContent).toBe("checkout")
-    })
-    it("should not add page type element to the page if value is not provided", async () => {
-      init({
-        merchantId: "shopify-123"
-      })
-
-      const el = document.querySelector(".nosto_page_type")
-      expect(el).toBeNull()
-    })
   })
 })
