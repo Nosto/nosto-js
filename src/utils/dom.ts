caae32dab96cb6e135c7d7b6e446518c2ecd4423
Timo Westkämper <timo.westkamper@nosto.com>
2024-10-10 08:49:54 +0300

    Drop setPageType functionality

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -1,15 +1,4 @@
-import { API, PageType } from "../client/nosto"
-
-export function setPageType(pageType: PageType) {
-  document.querySelectorAll(".nosto_page_type").forEach(el => el.remove())
-
-  const div = document.createElement("div")
-  div.classList.add("nosto_page_type")
-  div.style.display = "none"
-  div.textContent = pageType
-  div.translate = false
-  document.body.appendChild(div)
-}
+import { API } from "../client/nosto"
 
 export function initNostoStub() {
   window.nostojs =
