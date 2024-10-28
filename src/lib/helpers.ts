e58f4a0a37432cc164abcbcbff6606107400cb8b
Maija Lintunokka <maija.lintunokka@nosto.com>
2024-10-28 11:41:24 +0200

    chore: add more typedoc comments

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,15 +1,26 @@
 import { Settings } from "../client/nosto"
 
+/**
+ * Checks if Nosto is loaded on the page.
+ */
 export function isNostoLoaded() {
   return typeof window.nosto !== "undefined"
 }
 
+/**
+ * Reloads Nosto with the given settings, useful for development purposes.
+ *
+ * @param settings Settings to override.
+ */
 export function reloadNosto(settings: Partial<Settings>) {
   if (window.nosto) {
     window.nosto.reload(settings)
   }
 }
 
+/**
+ * Gets the correctly Nosto object.
+ */
 export function getNostoWindow() {
   return window.nosto
 }
