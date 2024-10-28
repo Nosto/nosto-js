17d6b4033840f890b29ce12cbcf731029b39c648
Maija Lintunokka <maija.lintunokka@nosto.com>
2024-10-28 11:36:38 +0200

    chore: add some typedoc comments

--- src/testing/mockNostojs.ts
+++ src/testing/mockNostojs.ts
@@ -2,6 +2,9 @@ import { API, nostojs } from "../client/nosto"
 
 let originalNostojs: nostojs
 
+/**
+ * Replaces the `nostojs` and `window.nostojs` functions with a mock implementation.
+ */
 export function mockNostojs(mock?: Partial<API>) {
   if (!originalNostojs) {
     originalNostojs = window.nostojs
@@ -9,10 +12,16 @@ export function mockNostojs(mock?: Partial<API>) {
   window.nostojs = (callback: (api: API) => unknown) => callback(mock as API)
 }
 
+/**
+ * Restores the original implementation for `nostojs` and `window.nostojs`.
+ */
 export function restoreNostojs() {
   window.nostojs = originalNostojs
 }
 
+/**
+ * Clears out all Nosto related globals from the window object.
+ */
 export function clearNostoGlobals() {
   Object.assign(window, {
     // clearing Nosto iframe window handle
