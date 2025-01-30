95c3c791c74ab39f5003fdc5afd4455b6f15ddea
Maija Lintunokka <maija.lintunokka@nosto.com>
2025-01-30 14:48:01 +0200

    fix: add typedoc comment

--- src/performance/measure.ts
+++ src/performance/measure.ts
@@ -1,5 +1,13 @@
 type ValidName = `nosto.${string}`
 
+/**
+ * Measures the time between the start and end of a given operation.
+ *
+ * Name of the measurement should start with `nosto.`.
+ * A measurement done with this utility function will appear in Debug Toolbar.
+ *
+ * @returns A function that stops the measurement.
+ */
 export function measure(name: ValidName) {
   performance.mark(`${name}.start`)
 
